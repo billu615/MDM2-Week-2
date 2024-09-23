360dafa0c301a905e7a80175f800d3d01f4e512a
Aaron-YUE-Ma <159254977+Aaron-YUE-Ma@users.noreply.github.com>
2024-09-23 16:06:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 entered = []
 
-score = 0
-high = 0
+score = 1
+high = 2
 lives = 2
 playing = True
 
